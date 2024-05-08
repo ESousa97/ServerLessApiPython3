6a99e887ef8ae41d7ce1e2c2e38be7f357891fd9
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-05-08 11:08:40 -0300

    att

--- next.config.js
+++ next.config.js
@@ -4,8 +4,12 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'production' ? 'https://your-vercel-deployment-url/api/:path*' : 'http://localhost:5328/:path*',
+        destination: process.env.NODE_ENV === 'production' ? 'https://server-less-api-python3.vercel.app/api/:path*' : 'http://localhost:5002/:path*',
       },
+      {
+        source: '/upload-excel*',
+        destination: process.env.NODE_ENV === 'production' ? 'https://server-less-api-python3.vercel.app/upload-excel*' : 'http://localhost:5002/upload-excel',
+      }
     ]
   },
 };
